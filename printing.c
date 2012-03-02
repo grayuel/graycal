fb3a6ead8c1d613f5ad0e6141dad1c5d1c866a8b
samuel <samgray@sonic.net>
2012-03-01 21:57:29 -0800

    moved print settings to a struct

--- printing.c
+++ printing.c
@@ -1,29 +1,76 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define CGRAY "${color2}"
 #define CWHITE "${color0}"
-#define CGREEN "${color1}"
+#define CHL "${color1}"
 #define CRESET "${color0}"
 
 #define GRAY "\033[0;37m"
 #define WHITE "\033[0;97m"
-#define GREEN "\033[0;32m"
+#define HL "\033[0;32m"
 #define RESET "\033[0m"
+
+
+
+
+//Settings for printing
+struct printSettings {
+    int startDay;
+    int today;
+    char *title;
+    char *todayColor;
+    char *future;
+    char *reset;
+};
+
+//Creates the struct that holds all print settings
+struct printSettings *createSettings(int count, int initDay, int outType)
+{
+    struct printSettings *temp = malloc(sizeof(struct printSettings));
+
+    temp->startDay = count;
+    temp->today = initDay;
+    if(!outType){
+        temp->title = GRAY;
+        temp->todayColor = HL;
+        temp->future = WHITE;
+        temp->reset = RESET;
+    }
+    else {
+        temp->title = CGRAY;
+        temp->todayColor = CHL;
+        temp->future = CWHITE;
+        temp->reset = CRESET;
+    }
+
+    return temp;
+}
+
+//Destroys the struct that holds all print settings
+void destroySettings(struct printSettings *temp)
+{
+    free(temp);
+}
+
+
+
 
 void printHeading(char *titleColor)
 {
     printf("%sSun Mon Tue Wed Thu Fri Sat\n", titleColor);
 }
 
-void printWeeks(int count, int initDay, char *todayColor, char *futureColor, char *reset)
+void printWeeks(struct printSettings *a)
 {
     int i;
     int todayReached = 0;
+    int count = a->startDay;
 
     for(i = 0; i < 35; i++) {
-        if(!todayReached && dow(initDay) == dow(count)) {
-            printf("%s %2d %s", todayColor, num2day(count), futureColor);
-                todayReached = 1;
+        if(!todayReached && dow(a->today) == dow(count)) {
+            printf("%s %2d %s", a->todayColor, num2day(a->today), a->future);
+            todayReached = 1;
         }
         else
             printf(" %2d ", num2day(count));
@@ -31,15 +78,14 @@ void printWeeks(int count, int initDay, char *todayColor, char *futureColor, cha
         if(dow(count++) == 6)
             printf("\n");
     }
-    printf("%s", reset);
+    printf("%s", a->reset);
 }
 
 void print(int count, int initDay, int outType)
 {
-    char *todayColor = GREEN;
-    char *titleColor = GRAY;
-    char *futureColor = WHITE;
-    char *reset = RESET;
-    printHeading(titleColor);
-    printWeeks(count, initDay, todayColor, futureColor, reset);
+    struct printSettings *setPrint = createSettings(count, initDay, outType);
+
+    printHeading(setPrint->title);
+    printWeeks(setPrint);
+    destroySettings(setPrint);
 }
